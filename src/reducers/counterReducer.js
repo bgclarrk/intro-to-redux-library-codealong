132a6b262f12ab875728be39148ea60da017ef77
+ <+>
2021-10-30 13:01:23 -0700

    intro to redux library codealong

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -3,13 +3,16 @@ export default function counterReducer(
 		clicks: 0
 	},
 	action
-) {
+){
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log("Current state.clicks %s", state.clicks);
+			console.log("Updating state.clicks to %s", state.clicks + 1);
 			return {
 				clicks: state.clicks + 1
 			}
 		default:
+			console.log("Initial state.clicks: %s", state.clicks)
 			return state;
 	}
 }
